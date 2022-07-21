aa9b8bf343480f091cebfcedb57ee8877768983d
Jasmeet Arora <jasmeet.arora@notarize.com>
2022-07-21 14:16:09 -0400

    update link

--- website/about-me/src/components/haha/index.tsx
+++ website/about-me/src/components/haha/index.tsx
@@ -5,7 +5,10 @@ class Haha extends React.PureComponent {
   public render(): JSX.Element {
     return (
       <div>
-        <a href="https://github.com/scala/scala/pull/5310" target="_blank">
+        <a
+          href="https://github.com/scala/scala/pull/5310#pullrequestreview-90959963"
+          target="_blank"
+        >
           my proudest open source contribution
         </a>
       </div>
