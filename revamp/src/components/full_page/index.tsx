9d057a7048c3d2ea994e506631af41fb9acaa552
jasmeet107 <jasmeet.arora@notarize.com>
2023-05-17 14:03:36 -0400

    figuring stuff out

--- revamp/src/components/full_page/index.tsx
+++ revamp/src/components/full_page/index.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
 import * as styles from "./styles.module.scss";
-import "@fontsource/sacramento";
 import "@fontsource/commissioner/300.css";
 import "@fontsource/lobster-two";
 import Title from "../title";
 import About from "../about";
-
-import "@fontsource/sacramento";
 import "@fontsource/commissioner";
 
 const FullPage = () => {
